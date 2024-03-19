b19e06e69801266441269bd68acd3411b28c1151
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-19 22:54:09 +0800

    Aviasales App #1 - Верстка

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,10 +1,41 @@
+import { useState } from 'react';
 import classes from '../../index.module.scss';
 const Tabs = () => {
+  const [activeCard, setActiveCard] = useState(1);
+  const onTabClick = (e) => {
+    switch (e.target.id) {
+      case '1':
+        setActiveCard(1);
+        break;
+      case '2':
+        setActiveCard(2);
+        break;
+      case '3':
+        setActiveCard(3);
+        break;
+    }
+  };
+
+  const TabButton = (props) => {
+    const { id, title } = props;
+    return (
+      <button
+        className={
+          activeCard === id ? `${classes['tabs__button']} ${classes['tabs__button_active']}` : classes['tabs__button']
+        }
+        onClick={onTabClick}
+        id={id}
+      >
+        {title}
+      </button>
+    );
+  };
+
   return (
     <div className={classes.tabs}>
-      <button className={`${classes['tabs__button']} ${classes['tabs__button_active']}`}>самый дешевый</button>
-      <button className={classes['tabs__button']}>самый быстрый</button>
-      <button className={classes['tabs__button']}>оптимальный</button>
+      <TabButton title="самый дешевый" id={1} />
+      <TabButton title="самый быстрый" id={2} />
+      <TabButton title="оптимальный" id={3} />
     </div>
   );
 };
