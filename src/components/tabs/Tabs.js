fd7f686a7e91ab215f65c83237ac11bba58d0926
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-20 20:25:31 +0800

    убрал логику из табов

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,32 +1,9 @@
-import { useState } from 'react';
-
 import classes from '../../index.module.scss';
 const Tabs = () => {
-  const [activeCard, setActiveCard] = useState(1);
-  const onTabClick = (e) => {
-    switch (e.target.id) {
-      case '1':
-        setActiveCard(1);
-        break;
-      case '2':
-        setActiveCard(2);
-        break;
-      case '3':
-        setActiveCard(3);
-        break;
-    }
-  };
-
   const TabButton = (props) => {
     const { id, title } = props;
     return (
-      <button
-        className={
-          activeCard === id ? `${classes['tabs__button']} ${classes['tabs__button_active']}` : classes['tabs__button']
-        }
-        onClick={onTabClick}
-        id={id}
-      >
+      <button className={`${classes['tabs__button']} ${classes['tabs__button_active']}`} id={id}>
         {title}
       </button>
     );
