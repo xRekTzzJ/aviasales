d8028e297b5ee50225221ae7c1c9a88f53197968
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-21 12:24:26 +0800

    заработало

--- src/components/tab-button/TabButton.js
+++ src/components/tab-button/TabButton.js
@@ -1,26 +1,16 @@
-import { bindActionCreators, configureStore } from '@reduxjs/toolkit';
-import { useState } from 'react';
-
 import classes from '../../index.module.scss';
-import * as actions from '../actions/actions';
-import reducer from '../reducer/reducer';
 
-const TabButton = ({ id, title, name }) => {
-  const store = configureStore({ reducer });
-  const { dispatch } = store;
-  const { nameAction } = bindActionCreators(actions, dispatch);
-
-  const [styles, setStyles] = useState(`${classes['tabs__button']}`);
-  const update = () => {
-    setStyles(
-      store.getState() === name
-        ? `${classes['tabs__button']} ${classes['tabs__button_active']}`
-        : `${classes['tabs__button']}`
-    );
-  };
-  store.subscribe(update);
+const TabButton = ({ id, title, name, nameAction, active }) => {
+  let styles =
+    active === name ? `${classes['tabs__button']} ${classes['tabs__button_active']}` : `${classes['tabs__button']}`;
   return (
-    <button className={styles} id={id} onClick={() => nameAction(name)}>
+    <button
+      className={styles}
+      id={id}
+      onClick={() => {
+        nameAction(name);
+      }}
+    >
       {title}
     </button>
   );
--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,12 +1,24 @@
+import { bindActionCreators, configureStore } from '@reduxjs/toolkit';
+import { useState } from 'react';
+
 import classes from '../../index.module.scss';
+import * as actions from '../actions/actions';
+import reducer from '../reducer/reducer';
 import TabButton from '../tab-button';
 
 const Tabs = () => {
+  const store = configureStore({ reducer });
+  const { dispatch } = store;
+  const { nameAction } = bindActionCreators(actions, dispatch);
+  const [active, setActive] = useState('null');
+  store.subscribe(() => {
+    setActive(store.getState());
+  });
   return (
     <div className={classes.tabs}>
-      <TabButton title="самый дешевый" name={'cheapest'} />
-      <TabButton title="самый быстрый" name={'fastest'} />
-      <TabButton title="оптимальный" name={'optimal'} />
+      <TabButton title="самый дешевый" nameAction={nameAction} name={'cheapest'} active={active} />
+      <TabButton title="самый быстрый" nameAction={nameAction} name={'fastest'} active={active} />
+      <TabButton title="оптимальный" nameAction={nameAction} name={'optimal'} active={active} />
     </div>
   );
 };
